396f05da21539489431c5372a7b4101dd199e197
Radek Krochmal <radek.krochmall@gmail.com>
2024-09-29 01:34:07 +0200

    add toast for checking email

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,21 +1,32 @@
+"use client";
 import { signInAction } from "@/app/actions";
 import { FormMessage, Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import { useEffect } from "react";
+import { useToast } from "@/hooks/use-toast";
 
 export default function Login({ searchParams }: { searchParams: Message }) {
-  console.log({ searchParams });
+  const { toast } = useToast();
 
+  useEffect(() => {
+    if (Object.keys(searchParams)[0] === "success") {
+      toast({
+        title: "Sukces!",
+        description: "Link do logowania został wysłany na Twój adres email.",
+        variant: "default",
+      });
+    }
+  }, [searchParams, toast]);
   return (
     <form className="flex flex-col min-w-64 m-auto">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">Zaloguj się</h1>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
+        <Label htmlFor="email">Adres Email</Label>
+        <Input name="email" placeholder="twójemail@przykład.pl" required />
+        <SubmitButton pendingText="Logowanie..." formAction={signInAction}>
+          Zaloguj się
         </SubmitButton>
         <FormMessage message={searchParams} />
       </div>
