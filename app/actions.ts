26ed8bc6b78c291f7072b4bad4f442dad3a383db
furas <szymon.calus@zs6sobieski.edu.pl>
2024-09-28 23:41:29 +0200

    Add most fields to AI Logic

--- app/actions.ts
+++ app/actions.ts
@@ -91,16 +91,31 @@ export const askAI = async (formData: FormData) => {
     schema: z.object({
       answer: z.string(),
       jailbreak_attempt: z.boolean().describe("Czy pytanie jest próbą wyjścia poza temat"),
-      point_of_interest: z.enum(["Umowa", "Zmiana Umowy", "Orzeczenie Sądu lub Ugoda", "Inne"])
-      .describe("Przedmiot opodatkowania"),
+
+      // SECTION A
       date_of_action: z.string().describe("Data wykonania czynności, jeśli jej nie ma, wpisz 'brak'"),
-      PESEL: z.number().describe("PESEL osoby, której dotyczy pytanie. Sprawdź, czy na pewno jest poprawny. JEżeli nie, zapisz pesel jako 0"),
       tax_office: z.string().describe("Nazwa urzędu skarbowego, w którym jest zarejestrowana osoba, której dotyczy pytanie"),
       purpose_of_action: z.enum(["Złożenie Deklaracji", "Korekta Deklaracji", "Brak"])
-      .describe("Cel czynności, której dotyczy pytanie"),
-      subject_submitting_the_action: z.enum(["Podmiot zobowiązany", "Strona umowy zamiany", "Wspólnik Spółki", "Pożyczkobiorca", "Inny"]).describe("Podmiot składający deklarację"),
-      taxpayer_type: z.enum(["Podmiot nie będący Osobą Fizyczną", "Osoba Fizyczna", "Brak"]).describe("Typ podatnika. Nie jest to informacja obowiązkowa do zgłoszenia"),
-      NIP: z.number().describe("NIP podatnika.Sprawdź, czy na pewno jest poprawny. JEżeli nie, zapisz pesel jako 0"),
+      .describe("Cel czynności, której dotyczy pytanie."),
+      
+      // SECTION B
+      taxpayer: z.enum(["Podmiot zobowiązany solidarnie do zapłaty podatku", "Strona umowy zamiany", "Wspólnik spółki cywilnej", "Podmiot, o którym mowa w art. 9 pkt 10 lit. b ustawy (pożyczkobiorca)", "Inny podmiot", "Brak"]).describe("Rodzaj podatnika, który składa deklarację"),
+      taxpayer_type: z.enum(["Podmiot nie będący Osobą Fizyczną", "Osoba Fizyczna", "Brak"]).describe("Typ podatnika."),
+      NIP: z.number().describe("NIP podatnika. Sprawdź, czy na pewno jest poprawny. Jeżeli nie, zapisz pesel jako 0"),
+      PESEL: z.number().describe("PESEL osoby, której dotyczy pytanie. Sprawdź, czy na pewno jest poprawny. Jeżeli nie, zapisz pesel jako 0"),
+      
+      // SECTION B1 (taxpayer_type = "Podmiot nie będący Osobą Fizyczną")
+      company_name: z.string().describe("Pełna nazwa firmy podatnika"),
+      company_shortname: z.string().describe("Skrócona nazwa firmy podatnika"),
+      
+      // SECTION B2 (taxpayer_type = "Osoba Fizyczna")
+      taxpayer_firstname: z.string().describe("Imię podatnika"),
+      taxpayer_lastname: z.string().describe("Nazwisko podatnika"),
+      taxpayer_date_of_birth: z.string().describe("Data urodzenia podatnika"),
+      taxpayer_father_name: z.string().describe("Imię ojca podatnika"),
+      taxpayer_mother_name: z.string().describe("Imię matki podatnika"),
+      
+      // SECTION B SHARED
       taxpayer_country: z.string().describe("Kraj podatnika"),
       taxpayer_voivodeship: z.string().describe("Województwo podatnika"),
       taxpayer_county: z.string().describe("Powiat podatnika"),
@@ -110,6 +125,10 @@ export const askAI = async (formData: FormData) => {
       taxpayer_house_number: z.string().describe("Numer domu podatnika."),
       taxpayer_apartment_number: z.string().describe("Numer mieszkania podatnika. Nie jest to informacja obowiązkowa do zgłoszenia"),
       taxpayer_postal_code: z.string().describe("Kod pocztowy podatnika"),
+      
+      // SECTION C
+      point_of_interest: z.enum(["Umowa", "Zmiana Umowy", "Orzeczenie Sądu lub Ugoda", "Inne"])
+      .describe("Przedmiot opodatkowania"),
       short_action_description: z.string().describe("Rodzaj Umowy, przedmiot i jego cechy charakterystyczne"),
 
     }),
